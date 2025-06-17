e6481e4e809a022190339d06e944dad268a20d7e
Yaël Dillies <yael.dillies@gmail.com>
2025-06-17 21:30:35 +0000

    fix

--- Mathlib/CategoryTheory/Monoidal/Cartesian/Mon_.lean
+++ Mathlib/CategoryTheory/Monoidal/Cartesian/Mon_.lean
@@ -58,15 +58,15 @@ instance [IsCommMon M] : IsMon_Hom μ[M] where
 
 instance : CartesianMonoidalCategory (Mon_ C) where
   isTerminalTensorUnit := .ofUniqueHom (fun M ↦ ⟨toUnit _⟩) fun M f ↦ by ext; exact toUnit_unique ..
-  fst M N := .mk (fst M.X N.X) (by simp [toUnit_unique _ (𝟙 _)])
-  snd M N := .mk (snd M.X N.X) (by simp [toUnit_unique _ (𝟙 _)])
+  fst M N := .mk (fst M.X N.X)
+  snd M N := .mk (snd M.X N.X)
   tensorProductIsBinaryProduct M N :=
-    BinaryFan.IsLimit.mk _ (fun {T} f g ↦ .mk (lift f.hom g.hom)
-      (by dsimp; ext <;> simp [toUnit_unique _ (𝟙 _)])
-      (by dsimp; ext <;> simp [toUnit_unique _ (𝟙 _), ← tensor_comp_assoc]))
+    BinaryFan.IsLimit.mk _ (fun {T} f g ↦ ⟨lift f.hom g.hom⟩)
       (by aesop_cat) (by aesop_cat) (by aesop_cat)
   fst_def M N := by ext; simp [fst_def]; congr
   snd_def M N := by ext; simp [snd_def]; congr
+
+variable {M N₁ N₂ : Mon_ C}
 
 @[simp] lemma lift_hom (f : M ⟶ N₁) (g : M ⟶ N₂) : (lift f g).hom = lift f.hom g.hom := rfl
 @[simp] lemma fst_hom (M N : Mon_ C) : (fst M N).hom = fst M.X N.X := rfl
