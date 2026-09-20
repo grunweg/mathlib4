44e2ecd58f536a21e30cfd6228aedd0073876dd3
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-20 16:09:39 +0200

    chore(Analysis/Calculus/InverseFunctionTheorem): rename (anti)Lipschitz -> (anti)LipschitzWith in lemma names
    Part of #42869.

--- Mathlib/Analysis/Calculus/InverseFunctionTheorem/ApproximatesLinearOn.lean
+++ Mathlib/Analysis/Calculus/InverseFunctionTheorem/ApproximatesLinearOn.lean
@@ -104,17 +104,20 @@ theorem approximatesLinearOn_iff_lipschitzOnWith {f : E → F} {f' : E →L[𝕜
 alias ⟨lipschitzOnWith, _root_.LipschitzOnWith.approximatesLinearOn⟩ :=
   approximatesLinearOn_iff_lipschitzOnWith
 
-theorem lipschitz_sub (hf : ApproximatesLinearOn f f' s c) :
+theorem lipschitzWith_sub (hf : ApproximatesLinearOn f f' s c) :
     LipschitzWith c fun x : s => f x - f' x :=
   hf.lipschitzOnWith.to_restrict
 
-protected theorem lipschitz (hf : ApproximatesLinearOn f f' s c) :
+protected theorem lipschitzWith (hf : ApproximatesLinearOn f f' s c) :
     LipschitzWith (‖f'‖₊ + c) (s.domRestrict f) := by
   simpa only [domRestrict_apply, add_sub_cancel] using!
-    (f'.lipschitzWith.restrict s).add hf.lipschitz_sub
+    (f'.lipschitzWith.restrict s).add hf.lipschitzWith_sub
+
+@[deprecated (since := "2026-09-20")] alias lipschitz_sub := ApproximatesLinearOn.lipschitzWith_sub
+@[deprecated (since := "2026-09-20")] alias lipschitz := ApproximatesLinearOn.lipschitzWith
 
 protected theorem continuous (hf : ApproximatesLinearOn f f' s c) : Continuous (s.domRestrict f) :=
-  hf.lipschitz.continuous
+  hf.lipschitzWith.continuous
 
 protected theorem continuousOn (hf : ApproximatesLinearOn f f' s c) : ContinuousOn f s :=
   continuousOn_iff_continuous_domRestrict.2 hf.continuous
@@ -309,16 +312,18 @@ variable {f' : E ≃L[𝕜] F} {s : Set E} {c : ℝ≥0}
 
 local notation "N" => ‖(f'.symm : F →L[𝕜] E)‖₊
 
-protected theorem antilipschitz (hf : ApproximatesLinearOn f (f' : E →L[𝕜] F) s c)
+protected theorem antilipschitzWith (hf : ApproximatesLinearOn f (f' : E →L[𝕜] F) s c)
     (hc : Subsingleton E ∨ c < N⁻¹) : AntilipschitzWith (N⁻¹ - c)⁻¹ (s.domRestrict f) := by
   rcases hc with hE | hc
   · exact AntilipschitzWith.of_subsingleton
-  convert! (f'.antilipschitz.domRestrict s).add_lipschitzWith hf.lipschitz_sub hc
+  convert! (f'.antilipschitz.domRestrict s).add_lipschitzWith hf.lipschitzWith_sub hc
   simp [domRestrict]
+
+@[deprecated (since := "2026-09-20")] alias antilipschitz := ApproximatesLinearOn.antilipschitzWith
 
 protected theorem injective (hf : ApproximatesLinearOn f (f' : E →L[𝕜] F) s c)
     (hc : Subsingleton E ∨ c < N⁻¹) : Injective (s.domRestrict f) :=
-  (hf.antilipschitz hc).injective
+  (hf.antilipschitzWith hc).injective
 
 protected theorem injOn (hf : ApproximatesLinearOn f (f' : E →L[𝕜] F) s c)
     (hc : Subsingleton E ∨ c < N⁻¹) : InjOn f s :=
@@ -377,7 +382,7 @@ theorem to_inv (hf : ApproximatesLinearOn f (f' : E →L[𝕜] F) s c) (hc : Sub
     _ ≤ N * (c * (((N⁻¹ - c)⁻¹ : ℝ≥0) * ‖A y' - A x'‖)) := by
       gcongr
       rw [← dist_eq_norm, ← dist_eq_norm]
-      exact (hf.antilipschitz hc).le_mul_dist ⟨y', y's⟩ ⟨x', x's⟩
+      exact (hf.antilipschitzWith hc).le_mul_dist ⟨y', y's⟩ ⟨x', x's⟩
     _ = (N * (N⁻¹ - c)⁻¹ * c : ℝ≥0) * ‖A x' - A y'‖ := by
       simp only [norm_sub_rev, NNReal.coe_mul]; ring
 
